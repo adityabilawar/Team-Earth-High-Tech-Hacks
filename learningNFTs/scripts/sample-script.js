ccfbb1476e866df161d2e34293e0970aae5476ec
Aditya Bilawar <adityabilawar@gmail.com>
2022-05-22 07:02:21 -0400

    updated UI

--- learningNFTs/scripts/sample-script.js
+++ learningNFTs/scripts/sample-script.js
@@ -1,17 +1,8 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+   //await hre.run('compile');
 
   // We get the contract to deploy
   const LearningNFTs = await hre.ethers.getContractFactory("LearningNFTs");
@@ -22,8 +13,7 @@ async function main() {
   console.log("LearningNFTs deployed to:", learnnfts.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
---
+++ learningNFTs/src/components/DeSoPostNFT.js
@@ -0,0 +1,89 @@
+import Deso from 'deso-protocol';
+const deso = new Deso();
+const request = {
+  "UpdaterPublicKeyBase58Check": null,
+  "NFTPostHashHex": "be84338d248394f9ef194c01054039a51667420a7fb91fb838c2445f786432b6",
+  "NumCopies": 1,
+  "NFTRoyaltyToCreatorBasisPoints": 100,
+  "NFTRoyaltyToCoinBasisPoints": 100,
+  "HasUnlockable": false,
+  "IsForSale": false,
+  "MinFeeRateNanosPerKB": 1000
+};
+ const response = await deso.nft.createNft(request); 
+
+
+
+ import LearningWalletBalance from './LearningWalletBalance';
+import { useEffect, useState } from 'react';
+
+import { ethers } from 'ethers';
+import LearningNFTs from './artifacts/contracts/LearningNFT.sol/LearningNFTs.json';
+
+const contractAddress = 'f54f090afa8ef07f076b1ab3824d874e';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// get the end user
+const signer = provider.getSigner();
+
+// get the smart contract
+const contract = new ethers.Contract(contractAddress, LearningNFT.abi, signer);
+
+
+function Home() {
+
+  const [totalMinted, setTotalMinted] = useState(0);
+  useEffect(() => {
+    getCount();
+  }, []);
+
+  const getCount = async () => {
+    const count = await contract.count();
+    console.log(parseInt(count));
+    setTotalMinted(parseInt(count));
+  };
+
+  return (
+    <div>
+        <LearningWalletBalance />
+
+        {Array(totalMinted + 1)
+        .fill(0)
+        .map((_, i) => (
+            <div key={i}>
+            <NFTImage tokenId={i} getCount={getCount} />
+            </div>
+        ))}
+    </div>
+  );
+}
+
+export default Home;
+
+
+import { useState } from 'react';
+import { ethers } from 'ethers';
+
+function LearningWalletBalance() {
+
+    const [balance, setBalance] = useState();
+    
+    const getBalance = async () => {
+        const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const Learnerprovider = new ethers.providers.Web3Provider(window.ethereum);
+        const Studentsbalance = await Learnerprovider.getBalance(account);
+        setBalance(ethers.utils.formatEther(Studentsbalance));
+    };
+  
+    return (
+     <div className="card">
+        <div>
+             <h5>Your Balance: {Studentsbalance}</h5>
+             <button onClick={() => getBalance()}>Show My Balance</button>
+        </div>
+     </div>
+    );
+  };
+  
+ 
